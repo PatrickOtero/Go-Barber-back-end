1afcc23d5faa60027cd60a98f57e4b81525653e4
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-09-04 23:07:32 -0300

    tentativa de novo deploy no Railway

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes')
-const swaggerUi = require('swagger-ui-express')
 
 const app = express()
 
