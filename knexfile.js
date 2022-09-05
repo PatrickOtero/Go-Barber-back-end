adeca629ed1a0bde92fc9de978ac673b379f9fc0
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-09-04 23:05:08 -0300

    tentativa de novo deploy no Railway

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ module.exports = {
 
     client: 'pg',
     connection: {
-      user: process.env.DB_LOCAL_USER,
-      host: process.env.DB_LOCAL_HOST,
-      database: process.env.DB_LOCAL_DATABASE,
-      password: process.env.DB_LOCAL_PASSWORD,
+      user: process.env.DB_LOCAL_USER || process.env.DB_SERVER_USER,
+      host: process.env.DB_LOCAL_HOST || process.env.DB_SERVER_HOST,
+      database: process.env.DB_LOCAL_DATABASE || process.env.DB_SERVER_DATABASE,
+      password: process.env.DB_LOCAL_PASSWORD || process.env.DB_SERVER_PASSWORD,
     },
     migrations: {
       directory: "src/migrations",
