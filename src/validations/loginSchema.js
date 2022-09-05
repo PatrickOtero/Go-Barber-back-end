f0d3d9cf2dc21c271522710c68484a71aae307c3
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-09-04 22:34:23 -0300

    tentativa de novo deploy no Railway

--- src/validations/loginSchema.js
+++ src/validations/loginSchema.js
@@ -1,7 +1,7 @@
 const yup = require('./configurations')
 
 const loginSchema = yup.object().shape({
-  user_email: yup.string().email().required("O campo 'e-mail' é obrigatório"),
+  user_email: yup.string().email("Insira um e-mail em formato válido").required("O campo 'e-mail' é obrigatório"),
   user_password: yup
     .string()
     .required("O campo 'senha' é obrigatório")
